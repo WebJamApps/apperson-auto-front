9edd11789783f3f815998d54bf5919d58fcef1b7
Maria Sherman <chemmariasherman@gmail.com>
2017-04-19 11:58:26 -0400

    fixed the issue with babel

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import config from '../authConfig';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import * as Bluebird from 'bluebird';
-
+import 'babel-polyfill';
 // remove out if you don't want a Promise polyfill (remove also from webpack.config.js)
 Bluebird.config({ warnings: { wForgottenReturn: false } });
 
@@ -23,18 +23,18 @@ export async function configure(aurelia) {
   aurelia.use.plugin(PLATFORM.moduleName('aurelia-auth'), (baseConfig)=>{
     baseConfig.configure(config);
   });
-  
+
   // Uncomment the line below to enable animation.
   // aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
   // if the css animator is enabled, add swap-order="after" to all router-view elements
-  
+
   // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
   // aurelia.use.plugin(PLATFORM.moduleName('aurelia-html-import-template-loader'));
-  
+
   document.addEventListener('WebComponentsReady', function() {
     aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
   });
-  
+
   // await aurelia.start();
   // await aurelia.setRoot(PLATFORM.moduleName('app'));
 }

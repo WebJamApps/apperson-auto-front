080a4a1c121ca65de77822af6dd5e1ba9ff3c34f
dannymcwaves <dannymcwaves96@gmail.com>
2019-01-17 11:09:11 +0000

    app component fixes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 import {inject, bindable} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
-import {AppRouterConfig} from './app.router.config';
-import {FetchConfig} from 'aurelia-auth';
-import {AuthService} from 'aurelia-auth';
+import {FetchConfig, AuthService} from 'aurelia-auth';
 import {HttpClient} from 'aurelia-fetch-client';
-import {AppState} from './classes/AppState.js';
+
+import {AppRouterConfig} from './app.router.config';
+import {AppState} from './classes/AppState';
+
 System.import('isomorphic-fetch');
 System.import('whatwg-fetch');
 @inject(Router, FetchConfig, AuthService, AppRouterConfig, HttpClient, AppState)
 export class App {
-  constructor(router, fetchConfig, auth, appRouterConfig, httpClient, appState){
+  constructor(router, fetchConfig, auth, appRouterConfig, httpClient, appState) {
     this.router = router;
     this.appRouterConfig = appRouterConfig;
     this.fetchConfig = fetchConfig;
@@ -31,11 +32,11 @@ export class App {
   // @bindable
   // logoWidth = '200px'
 
-  get widescreen(){
+  get widescreen() {
     let iswidescreen = false;
-    let currentscreenwidth = document.documentElement.clientWidth;
+    const currentscreenwidth = document.documentElement.clientWidth;
     /* istanbul ignore else */
-    if (currentscreenwidth > 970){
+    if (currentscreenwidth > 970) {
       iswidescreen = true;
       //this.logoWidth = '200px';
       //  } else {
@@ -44,7 +45,7 @@ export class App {
     return iswidescreen;
   }
 
-  togglemenu(){
+  togglemenu() {
     if (this.fullmenu) {
       this.fullmenu = false;
       this.drawerWidth = '50px';
@@ -60,7 +61,7 @@ export class App {
     }
   }
 
-  logout(){
+  logout() {
     this.auth.setToken('');
     this.authenticated = false;
     this.auth.logout('/');
@@ -74,7 +75,7 @@ export class App {
   activate() {
     this.appRouterConfig.configure();
     this.configHttpClient();
-    if (this.auth.isAuthenticated()){
+    if (this.auth.isAuthenticated()) {
       this.authenticated = true;
       this.appState.setAuth(true);
       this.appState.setRoles(['dashboard']);
@@ -83,17 +84,16 @@ export class App {
     }
   }
 
-  configHttpClient(){
+  configHttpClient() {
     this.httpClient.configure(httpConfig => {
-      httpConfig
-      .withDefaults({
+      httpConfig.withDefaults({
         mode: 'cors',
         credentials: 'same-origin',
         headers: {
-          'Accept': 'application/json'
+          Accept: 'application/json'
         }
       })
-      .withInterceptor(this.auth.tokenInterceptor);
+        .withInterceptor(this.auth.tokenInterceptor);
     });
   }
 }

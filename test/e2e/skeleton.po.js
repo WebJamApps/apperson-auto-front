746445a022e02ebd2f8edaa0aa19b2e5a8922407
dannymcwaves <dannymcwaves96@gmail.com>
2019-01-17 11:10:49 +0000

    e2e test updated to avoid linter errors

--- test/e2e/skeleton.po.js
+++ test/e2e/skeleton.po.js
@@ -1,11 +1,10 @@
 export class PageObjectSkeleton {
-
   getCurrentPageTitle() {
     return browser.getTitle();
   }
 
   navigateTo(href) {
-    element(by.css('a[href="' + href + '"]')).click();
+    element(by.css(`a[href="${href}"]`)).click();
     return browser.waitForRouterComplete();
   }
 }
